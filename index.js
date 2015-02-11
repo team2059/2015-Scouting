0834518871764e47a7bf660f564ba5c0c857e687
CarlColglazier <carltcolglazier@gmail.com>
2015-02-10 21:10:39 -0500

    Prepare module function.

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var app = {};
 
-app.name = "2015-Scouting";
+app.name = "2015-Scouting :)";
 app.reserve = [""];
 
 app.start = function(THUMS) {
     this.data = THUMS.data;
     this.sockets = THUMS.sockets;
 };
+
+app.module = function(req,res,next) {
+    console.log("I tried my very best :)");
+    console.log(req.url.split("/")[1])
+    next();
+}
 
 module.exports = app;
