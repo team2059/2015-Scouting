c25b1674485a88291981ef9f2c80c453dad9d995
CarlColglazier <carltcolglazier@gmail.com>
2015-02-12 21:00:49 -0500

    Fix bugs
    + Load HTML from external file.
    + Split module functions for front end and back end.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-var app, scouting_modules,
+var app, admin_modules, module_html,
+    scouting_modules,
     scoutingSocket = require("./lib/sockets.js"),
     scoutingData = require("./lib/data.js"),
     fs = require("fs");
@@ -7,18 +8,51 @@ app = {};
 app.name = "2015-Scouting :)";
 app.reserve = ["","scout"];
 
+function openBody(cb) {
+    fs.readFile("content/apps/2015-Scouting/lib/modules.html", "utf8", function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (data.indexOf("_____") >= 0) {
+                module_html = data.split("_____");
+                for (var i = 0; i < module_html.length; i++) {
+                    if (module_html[i].indexOf("=====") >= 0) {
+                        module_html[i] = module_html[i].split("=====");
+                    }
+                }
+            } else {
+                module_html = [[],[]];
+            }
+        }
+        cb();
+    });
+}
+
+function loadModules(modules,num) {
+    var new_modules = [];
+    for (var x = 0; x < modules.length; x++) {
+        if (modules[x].html) {
+            if (module_html[num][modules[x].ord]) {
+                modules[x].content.body = module_html[num][modules[x].ord];
+            } else {
+                modules[x].content.body = "";
+            }
+        }
+        new_modules.push(modules[x]);
+    }
+    return new_modules;
+}
+
 function openScript() {
     fs.readFile('content/apps/2015-Scouting/scripts.js', function (err, data) {
         if (!err) {
-            scouting_modules = [
+            var module_list = [
                 {
                     "type": "html",
                     "html": true,
                     "class": "module half",
                     "title": "Match #<span class=\"match-number\">--</span>",
-                    "content": {
-                        "body": "<div class=\"panel\"><input id=\"input-name\"><select name=\"position\" id=\"robot-id\"><option value=null></option></select></div>"
-                    },
+                    "content": {},
                     "ord": 1
                 },
                 {
@@ -33,34 +67,41 @@ function openScript() {
                     "html": true,
                     "class": "module half",
                     "title": "Totes",
-                    "content": {
-                        "body": "<div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-6\" /><input type=\"number\" value=\"0\" name=\"tt6\" id=\"tt6\" class=\"number tote\"></input><input type=\"button\" class=\"increase\" value=\"+6\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-5\" /><input type=\"number\" value=\"0\" name=\"tt5\" id=\"tt5\" class=\"number tote\"></input><input type=\"button\" class=\"increase\" value=\"+5\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-4\" /><input type=\"number\" value=\"0\" name=\"tt4\" id=\"tt4\" class=\"number tote\"></input><input type=\"button\" class=\"increase\" value=\"+4\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-3\" /><input type=\"number\" value=\"0\" name=\"tt3\" id=\"tt3\" class=\"number tote\"></input><input type=\"button\" class=\"increase\" value=\"+3\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-2\" /><input type=\"number\" value=\"0\" name=\"tt2\" id=\"tt2\" class=\"number tote\"></input><input type=\"button\" class=\"increase\" value=\"+2\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-1\" /><input type=\"number\" value=\"0\" name=\"tt1\" id=\"tt1\" class=\"number tote\"></input><input type=\"button\" class=\"increase\" value=\"+1\" /></div>"
-                    },
+                    "content": {},
                     "ord": 3
                 }, {
                     "type": "html",
                     "html": true,
                     "class": "module half",
                     "title": "Bins",
-                    "content": {
-                        "body": "<div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-6\" /><input type=\"number\" value=\"0\" name=\"tb6\" id=\"tb6\" class=\"number bin\"></input><input type=\"button\" class=\"increase\" value=\"+6\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-5\" /><input type=\"number\" value=\"0\" name=\"tb5\" id=\"tb5\" class=\"number bin\"></input><input type=\"button\" class=\"increase\" value=\"+5\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-4\" /><input type=\"number\" value=\"0\" name=\"tb4\" id=\"tb4\" class=\"number bin\"></input><input type=\"button\" class=\"increase\" value=\"+4\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-3\" /><input type=\"number\" value=\"0\" name=\"tb3\" id=\"tb3\" class=\"number bin\"></input><input type=\"button\" class=\"increase\" value=\"+3\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-2\" /><input type=\"number\" value=\"0\" name=\"tb2\" id=\"tb2\" class=\"number bin\"></input><input type=\"button\" class=\"increase\" value=\"+2\" /></div><div class=\"panel\"><input type=\"button\" class=\"decrease\" value=\"-1\" /><input type=\"number\" value=\"0\" name=\"tb1\" id=\"tb1\" class=\"number bin\"></input><input type=\"button\" class=\"increase\" value=\"+1\" /></div>"
-                    },
+                    "content": {},
                     "ord": 4
                 }, {
                     "type": "html",
                     "html": true,
                     "class": "module half",
                     "title": "Coopertition",
-                    "content": {
-                        "body": ""
-                    },
+                    "content": {},
                     "ord": 5
                 },
                 {
                     "type": "script",
                     "script": true,
-                    "content": data
+                    "content": data.toString()
                 }];
+            scouting_modules = loadModules(module_list,0);
+            module_list = [
+            {
+                "type": "html",
+                "html": true,
+                "class": "module half",
+                "title": "Dashboard",
+                "content": {
+                    "body": "<div class=\"panel\">Test.</div>"
+                }
+            }
+            ];
+            admin_modules = loadModules(module_list,1);
         } else {
             console.error(err);
         }
@@ -75,17 +116,30 @@ app.start = function(THUMS) {
         "columns": [
             "event CHAR(6)", "round SMALLINT(3)", "r1 SMALLINT(4)", "r2 SMALLINT(4)", "r3 SMALLINT(4)", "b1 SMALLINT(4)", "b2 SMALLINT(4)", "b3 SMALLINT(4)", "rscore SMALLINT(3)", "bscore SMALLINT(3)"
         ]
+    }, function(err,row) {
+        self.data.createTable({ "name":"match_scouting",
+            "columns": [
+                "event CHAR(6) DEFAULT NULL", "round SMALLINT(3) DEFAULT NULL", "team SMALLINT(4) DEFAULT NULL", "scouter CHAR", "tt1 SMALLINT(2) DEFAULT 0", "tt2 SMALLINT(2) DEFAULT 0", "tt3 SMALLINT(2) DEFAULT 0", "tt4 SMALLINT(2) DEFAULT 0", "tt5 SMALLINT(2) DEFAULT 0", "tt6 SMALLINT(2) DEFAULT 0", "tb1 SMALLINT(2) DEFAULT 0", "tb2 SMALLINT(2) DEFAULT 0", "tb3 SMALLINT(2) DEFAULT 0", "tb4 SMALLINT(2) DEFAULT 0", "tb5 SMALLINT(2) DEFAULT 0", "tb6 SMALLINT(2) DEFAULT 0"
+            ]
+        }, function(err,row) {
+            self.data = scoutingData(self.data);
+            scoutingSocket.init(self.data);
+            openBody(openScript);
+            var sock = self.sockets.of("/scout");
+            sock.on("connection",scoutingSocket.start);
+        });
     });
-    self.data = scoutingData(self.data);
-    scoutingSocket.init(self.data);
-    openScript();
-    var sock = self.sockets.of("/scout");
-    sock.on("connection",scoutingSocket.start);
-};
+}
 
 app.module = function(req,res,next) {
-    for (var x in scouting_modules){
-        req.post_info.modules.push(scouting_modules[x]);
+    if (req.root === "") {
+        for (var x in scouting_modules){
+            req.post_info.modules.push(scouting_modules[x]);
+        }
+    } else {
+        for (var x in admin_modules) {
+            req.post_info.modules.push(admin_modules[x]);
+        }
     }
     next();
 };
